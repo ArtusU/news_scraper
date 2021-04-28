59990abbcaa2f55f34e15be733f9909dec0c63b5
ArtusU <artusrock@hotmail.com>
2021-04-28 13:44:50 +0100

    NewsItemListView and template

--- core/views.py
+++ core/views.py
@@ -2,5 +2,22 @@ from django.views import generic
 from .models import NewsItem
 
 class NewsItemListview(generic.ListView):
-    model = NewsItem
     template_name = 'news_item_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        qs = NewsItem.objects.all()
+
+        title = self.request.GET.get('title', None)
+        if title:
+            qs = qs.filter(title__icontains=title)
+
+        return qs.order_by("-publish_date")
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsItemListview, self).get_context_data(**kwargs)
+        count = NewsItem.objects.all().count()
+        context.update({
+            "total_count": count
+        })
+        return context
